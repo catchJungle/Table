c9f786335ea22d192dcfddd69bb63761f26cdbb3
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-09-03 17:34:48 +0900

    feat: mongo test code added

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+client = MongoClient("mongodb://43.202.55.187:27017/")
+db = client["mydatabase"]
+collection = db["user"]
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/mongo')
+def mongo():
+    collection.insert_one({"status":"opened"})
+
+    users = collection.find({})
+    return jsonify(users)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
